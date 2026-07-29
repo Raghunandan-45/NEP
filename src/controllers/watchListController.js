240095cac7d6c91cc2f3b2848cf54920870ebbc8
Raghunandan-45 <araghunandan2015@gmail.com>
2026-07-29 22:32:41 +0530

    Added update and delete operations

--- src/controllers/watchListController.js
+++ src/controllers/watchListController.js
@@ -44,4 +44,61 @@ const addToWatchList = async (req,res) =>{
 
 };
 
-export {addToWatchList};
+
+const removeFromWatchlist = async (req,res) =>{
+    const watchListItem = await prisma.watchListItems.findUnique({
+        where: {id: req.params.id},
+    });
+
+    if(!watchListItem){
+        return res.status(404).json({error:"Watchlistitem not found!"});
+    }
+
+    if(watchListItem.userId !== req.user.id){
+        return res.status(403).json({error:"Not allowed to update this watchlist item"});
+    }
+
+    await prisma.watchListItems.delete({
+        where: {id: req.params.id},
+    });
+
+    res.status(200).json({
+        status: "success",
+        message: "Movie removed from watchlist",
+    });
+};
+
+const updateInWatchList = async (req,res) =>{
+    const { status, rating, notes} = req.body;
+
+    const watchListItem = await prisma.watchListItems.findUnique({
+        where: {id: req.params.id},
+    })
+
+    if(!watchListItem){
+        return res.status(404).json({error:"WatchListItem not found!"});
+    }
+
+    if(watchListItem.userId !== req.user.id){
+        return res.status(403).json({error:"Not allowed to update this watchlist"});
+    }
+
+    const updateData = {};
+    if(status !== undefined) updateData.status = status.toUpperCase();
+    if(rating !== undefined) updateData.rating = rating;
+    if(notes !== undefined) updateData.notes = notes;
+
+    const updatedItem = await prisma.watchListItems.update({
+        where: {id: req.params.id},
+        data: updateData,
+    });
+
+    res.status(200).json({
+        status:"success",
+        data:{
+            watchListItem:updatedItem,
+        },
+    });
+};
+
+export {addToWatchList, removeFromWatchlist, updateInWatchList};
